e973788b638221172e0fbad8377bbf64df6c3dfd
Jonas Marques <jonas.marques2021.jm5@gmail.com>
2023-08-15 21:28:29 -0300

    feat: react lazy

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,14 @@
 import Footer from 'components/Footer';
 import Menu from 'components/Menu';
-import PaginaPadrao from 'components/PaginaPadrao';
-// import Cardapio from 'pages/Cardapio';
-import Inicio from 'pages/Inicio';
-import NotFound from 'pages/NotFound';
-import Prato from 'pages/Prato';
-import Sobre from 'pages/Sobre';
 import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const Cardapio = lazy(() => import('pages/Cardapio'));
+const PaginaPadrao = lazy(() => import('components/PaginaPadrao'));
+const Inicio = lazy(() => import('pages/Inicio'));
+const NotFound = lazy(() => import('pages/NotFound'));
+const Sobre = lazy(() => import('pages/Sobre'));
+const Prato = lazy(() => import('pages/Prato'));
 
 export default function AppRouter() {
   return (
@@ -26,8 +25,8 @@ export default function AppRouter() {
             <Route path='prato/:id' element={<Prato />} />
             <Route path='*' element={<NotFound />} />
           </Routes>
-          <Footer />
         </Suspense>
+        <Footer />
       </Router>
     </main>
   );
